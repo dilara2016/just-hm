49d05e4a2b9edc595c7ab0d5b3a6b82c94e7b64e
dilara2016 <m.123.hunain@gmail.com>
2024-10-26 10:24:24 +0200

    hm

---
+++ hm.py
@@ -0,0 +1,20 @@
+a = 500
+b = 232
+c = 4
+if a and b and c:
+ print("ALL the numbers have boolean value as True")
+else:
+  print("Atleast one numbers have boolean value as False")
+
+
+a = 100
+b = -100
+c = 0
+if a > 0 or b > 0:
+  print("Either of the numbers is greater than 0")
+else:
+ print("no number is greater than 0")
+if b > 0 or c > 0:
+  print("Either of the numbers is greater than 0")
+else:
+ print("no number is greater than 0")
